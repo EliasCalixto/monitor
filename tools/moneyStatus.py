1b842720d7326a8646390475509598a178329881
Elias Calixto <85374767+EliasCalixto@users.noreply.github.com>
2024-08-15 12:36:34 -0500

    srgg

--- tools/moneyStatus.py
+++ tools/moneyStatus.py
@@ -3,38 +3,9 @@ import pandas as pd
 
 
 try:
-    dataFrame = pd.read_excel("C:\\Users\\elias\\OneDrive\\Documentos\\Main.xlsx", sheet_name='data')
+    dataFrame = pd.read_excel("C:\\Users\\elias\\OneDrive\\Documentos\\Main.xlsx", sheet_name='IngresosyGastos', engine='openpyxl')
     allData = np.array(dataFrame)
 except:
-    dataFrame = pd.read_excel('/Users/darkesthj/Library/CloudStorage/OneDrive-Personal/Documentos/Main.xlsx', sheet_name='data')
+    dataFrame = pd.read_excel('/Users/darkesthj/Library/CloudStorage/OneDrive-Personal/Documentos/Main.xlsx', sheet_name='IngresosyGastos', engine='openpyxl')
     allData = np.array(dataFrame)
 
-def getCurrentMonth():
-    index = 0
-
-    while allData[:,0][index] != 0:
-        index += 1
-
-    currentMonth = allData[index-1]
-    
-    return currentMonth
-
-def getTotalBlue():
-    currentMonth = getCurrentMonth()
-    totalBlue = round(np.sum(currentMonth[1:3]),2)
-    return f'{totalBlue}'
-    
-def getTotalRed():
-    currentMonth = getCurrentMonth()
-    totalRed = round(np.sum(currentMonth[3:7]),2)
-    return f'{totalRed}'
-
-def getCurrentMoney():
-    currentMonth = getCurrentMonth()
-    currentMoney = round(currentMonth[8],2)
-    return f'{currentMoney}'
-    
-if __name__ == '__main__':
-    print(f'{getTotalBlue()}/{getTotalRed()}/{getCurrentMoney()}')
-    print(getTotalBlue())
-    print(getTotalRed())
