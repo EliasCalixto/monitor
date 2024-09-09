0aa669fc1c9dc04687afd2dd8b827de600e2a52c
Elias Calixto <85374767+EliasCalixto@users.noreply.github.com>
2024-09-09 01:11:52 -0500

    sgg

--- tools/moneyStatus.py
+++ tools/moneyStatus.py
@@ -9,3 +9,11 @@ except:
     dataFrame = pd.read_excel('/Users/darkesthj/Library/CloudStorage/OneDrive-Personal/Documentos/Main.xlsx', sheet_name='IngresosyGastos', engine='openpyxl')
     allData = np.array(dataFrame)
 
+def getTotalBlue():
+    return 'totalBlue'
+
+def getTotalRed():
+    return 'totalRed'
+
+def getCurrentMoney():
+    return 'currentMoney'
