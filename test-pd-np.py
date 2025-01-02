69def0888391a1060cd0093620f6a177f573798e
Elias Calixto <85374767+EliasCalixto@users.noreply.github.com>
2025-01-02 01:58:10 -0500

    01/01

--- test-pd-np.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-
-main_data_path = "/Users/darkesthj/Library/CloudStorage/OneDrive-Personal/Documentos/Main.xlsx"
-main_data = pd.read_excel(main_data_path)
-print(main_data.columns)
-
