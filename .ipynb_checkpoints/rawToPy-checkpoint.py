01af99bc3e2b1476947ff33f7350b32a64976fba
Elias Calixto <85374767+EliasCalixto@users.noreply.github.com>
2025-01-13 23:00:22 -0500

    01/13

--- .ipynb_checkpoints/rawToPy-checkpoint.py
+++ .ipynb_checkpoints/rawToPy-checkpoint.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pandas as pd
+
 main_path = "/Users/darkesthj/Library/CloudStorage/OneDrive-Personal/Documentos/Main.xlsx"
 
 
---
+++ rawToPy.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+main_path = "/Users/darkesthj/Library/CloudStorage/OneDrive-Personal/Documentos/Main.xlsx"
+
+df_raw = pd.read_excel(main_path, 'DataRaw')
+df_py = pd.read_excel(main_path, 'DataPy')
+
+for i in range(len(df_raw)):
+    if df
