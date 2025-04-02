477abfdc9d4b038247626d4b93e8665ecb3ea991
Elias Calixto <85374767+EliasCalixto@users.noreply.github.com>
2025-04-02 15:33:46 -0500

    04/01

--- .ipynb_checkpoints/setup-checkpoint.py
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -22,62 +22,62 @@ def get_current_money():
 # Get Totals
 def get_totals(date = 0):
     total_investment = round(np.sum(df['Investment'][date:current_row+1]), 2)
-    total_productivity = round(np.sum(df['Productivity'][date:current_row+1]), 2)
+    total_setup = round(np.sum(df['Setup'][date:current_row+1]), 2)
+    total_home = round(np.sum(df['Home'][date:current_row+1]), 2)
     total_studies = round(np.sum(df['Studies'][date:current_row+1]), 2)
     total_fixed = round(np.sum(df['Fixed'][date:current_row+1]), 2)
     total_clothes = round(np.sum(df['Clothes'][date:current_row+1]), 2)
-    total_10 = round(np.sum(df['10'][date:current_row+1]), 2)
     total_enjoy = round(np.sum(df['Enjoy'][date:current_row+1]), 2)
     total_others = round(np.sum(df['Others'][date:current_row+1]), 2)
     total_cashout = round(np.sum(df['Cashout'][date:current_row+1]), 2)
 
     group_of_categories = ['Investment',
-                           'Productivity',
+                           'Setup',
+                           'Home',
                            'Studies',
                            'Fixed',
                            'Clothes',
-                           '10%',
                            'Enjoy',
                            'Others',
                            'Cashout']
 
     group_of_totals = [total_investment, 
-                       total_productivity, 
+                       total_setup, 
+                       total_home, 
                        total_studies, 
                        total_fixed, 
                        total_clothes, 
-                       total_10, 
                        total_enjoy, 
                        total_others, 
                        total_cashout]
 
     investment_percent = round((total_investment/np.sum(group_of_totals))*100, 2)
-    productivity_percent = round((total_productivity/np.sum(group_of_totals))*100, 2)
+    setup_percent = round((total_setup/np.sum(group_of_totals))*100, 2)
+    home_percent = round((total_home/np.sum(group_of_totals))*100, 2)
     studies_percent = round((total_studies/np.sum(group_of_totals))*100, 2)
     fixed_percent = round((total_fixed/np.sum(group_of_totals))*100, 2)
     clothes_percent = round((total_clothes/np.sum(group_of_totals))*100, 2)
-    percent_10 = round((total_10/np.sum(group_of_totals))*100, 2)
     enjoy_percent = round((total_enjoy/np.sum(group_of_totals))*100, 2)
     others_percent = round((total_others/np.sum(group_of_totals))*100, 2)
     cashout_percent = round((total_cashout/np.sum(group_of_totals))*100, 2)
 
     group_of_percent = [investment_percent,
-                       productivity_percent,
+                       setup_percent,
+                       home_percent,
                        studies_percent,
                        fixed_percent,
                        clothes_percent,
-                       percent_10,
                        enjoy_percent,
                        others_percent,
                        cashout_percent]
 
     print(f'Results since {df["Date"][date]}')
     print(f'Investment: {total_investment} / {investment_percent}%')
-    print(f'Productivity: {total_productivity} / {productivity_percent}%')
+    print(f'Setup: {total_setup} / {setup_percent}%')
+    print(f'Home: {total_home} / {home_percent}%')
     print(f'Studies: {total_studies} / {studies_percent}%')
     print(f'Fixed: {total_fixed} / {fixed_percent}%')
     print(f'Clothes: {total_clothes} / {clothes_percent}%')
-    print(f'10: {total_10} / {percent_10}%')
     print(f'Enjoy: {total_enjoy} / {enjoy_percent}%')
     print(f'Others: {total_others} / {others_percent}%')
     print(f'Cashout: {total_cashout} / {cashout_percent}%')
