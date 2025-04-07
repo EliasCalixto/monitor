027eba4c9c048d82d04e4a927b5e4e5607d9b5b0
Elias Calixto <85374767+EliasCalixto@users.noreply.github.com>
2025-04-07 18:40:43 -0500

    04/07

--- updatedb.py
+++ updatedb.py
@@ -1,9 +1,38 @@
+import subprocess
 from setup import df, current_row
 
 
-# 1 Pass df to new_df
+# 1 clone df into new_df
 new_df = df.copy()
 
 for i in range(current_row + 1, len(new_df)):
-    df.drop(index=i, inpew_lace=True)
+    new_df.drop(index=i, inplace=True)
 
+list_new_df = new_df.values.tolist()
+
+# 2 Creating the SQL request
+query = f"""USE main_db
+DELETE FROM dbo.data_raw
+GO
+
+INSERT INTO dbo.data_raw (Id,DateInfo,Income,Setup,Home,Savings,Studies,Enjoy,Others,Fixed,Cashout,Cash)
+VALUES 
+"""
+
+values = []
+
+for i in range(len(list_new_df)):
+    values.append(f"({list_new_df[i][0]},'{list_new_df[i][1]}',{list_new_df[i][2]},'{list_new_df[i][3]}','{list_new_df[i][4]}',{list_new_df[i][5]},{list_new_df[i][6]},{list_new_df[i][7]},{list_new_df[i][8]},{list_new_df[i][9]},{list_new_df[i][10]},{list_new_df[i][11]})")
+
+query += ',\n'.join(values) + ";\nGO"
+
+query += f"\n\nSELECT * FROM dbo.data_raw\nGO"
+
+with open('update.sql', 'w') as f:
+    f.write(query)
+    
+# 3 Ejecuting the SQL request through terminal
+command = ["sqlcmd","-S","192.168.18.93,1433","-U","sa","-P","Ea989571287!","-d","main_db","-i","monitor/update.sql"]
+
+result = subprocess.run(command, shell=True, capture_output=True, text=True)
+print(result.stdout)
